2e59a64d57572c4fc122ac6f202312ba5de4eaad
funyatomo <funyatomo@gmail.com>
2019-07-30 03:11:22 +0900

    modified

--- alias.bat
+++ alias.bat
@@ -2,19 +2,19 @@
 
 
 if "%~1"=="" (
-  dir /b "C:\bin\*.bat"
+  dir /b "%USERPROFILE%\bin\*.bat"
 ) else (
   if not "%~3"=="" (
     echo ˆø”‚ª‘½‚·‚¬‚Ü‚·B
   ) else if "%~2"=="" (
-    cat "C:\bin\%1.bat"
+    cat "%USERPROFILE%\bin\%~1.bat"
   ) else (
-    if exist "C:\bin\%1.bat" (
+    if exist "%USERPROFILE%\bin\%~1.bat" (
       echo Šù‚É“o˜^Ï‚Ý‚Å‚·B
     ) else (
-      echo @echo off>"C:\bin\%1.bat"
-      echo rem alias command>>"C:\bin\%1.bat"
-      echo %~2>>"C:\bin\%1.bat"
+      echo @echo off>"%USERPROFILE%\bin\%~1.bat"
+      echo rem alias command>>"%USERPROFILE%\bin\%~1.bat"
+      echo %~2>>"%USERPROFILE%\bin\%~1.bat"
     )
   )
 )
--- unalias.bat
+++ unalias.bat
@@ -2,15 +2,15 @@
 
 
 if "%~1"=="" (
-  findstr /M "alias command" "C:\bin\*.bat" | find /v "alias.bat"
+  findstr /M "alias command" "%USERPROFILE%\bin\*.bat" | find /v "alias.bat"
 ) else (
   if not "%~2"=="" (
     echo ˆø”‚ª‘½‚·‚¬‚Ü‚·B
   ) else (
-    if not exist "C:\bin\%~1.bat" (
+    if not exist "%USERPROFILE%\bin\%~1.bat" (
       echo “o˜^‚³‚ê‚Ä‚¢‚Ü‚¹‚ñB
     ) else (
-      del /p "C:\bin\%~1.bat"
+      del /p "%USERPROFILE%\bin\%~1.bat"
     )
   )
 )
